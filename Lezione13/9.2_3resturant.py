9e85bd60ef3b691dae248aef54b539a3eb1bcbe6
WRRDRN <adriano.morra@outlook.it>
2025-04-07 09:44:00 +0200

    esercizio15th

---
+++ Lezione13/9.2_3resturant.py
@@ -0,0 +1,15 @@
+from diodio import Resturant
+
+
+res1 = Resturant("Da Antonio","Ethiopian")
+res2 = Resturant("Da Giovanni","Italian")
+res3 = Resturant("Da Sossio","Argentinian")
+
+res1.describe_resturant()
+print()
+res2.describe_resturant()
+print()
+res3.describe_resturant()
+print()
+
+
