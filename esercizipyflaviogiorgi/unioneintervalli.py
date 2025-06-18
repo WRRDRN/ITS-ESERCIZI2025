cac0b8879efc33b8f79d0c0379bd4346307d4951
WRRDRN <adriano.morra@outlook.it>
2025-06-18 12:02:30 +0200

    8384

--- esercizipyflaviogiorgi/unioneintervalli.py
+++ esercizipyflaviogiorgi/unioneintervalli.py
@@ -1,20 +1,25 @@
-'''Unione di Intervalli Sovrapposti
-Data una lista di intervalli chiusi rappresentati come liste di due elementi [start, end],
-scrivi una funzione merge_intervals(intervals) che restituisce una nuova lista di
-intervalli in cui tutti quelli sovrapposti sono stati fusi. Ogni intervallo soddisfa start <=
-end. La lista risultante deve essere ordinata per inizio intervallo e non devono esserci
-sovrapposizioni.
-Requisiti:
-● Input: una lista di liste, ad esempio [[1, 4], [2, 6], [8, 10], [15, 18]].
-● Se due intervalli si sovrappongono o si toccano (es. [1,4] e [4,5]), unirli in
-[1,5].
-● Restituisci una lista di intervalli fusi, ordinata per il valore di inizio.
-● Casi limite:
-○ Se l’input è vuoto, restituisci una lista vuota.
-○ Se è presente un solo intervallo, restituiscilo così com’è.
-Esempi:
-intervals = [[1, 3], [2, 6], [8, 10], [15, 18]]
-merge_intervals(intervals) # restituisce [[1, 6], [8, 10], [15,
-18]]
-intervals = [[1, 4], [4, 5]]
-merge_intervals(intervals) # restituisce [[1, 5]]'''
+def merge_intervals(intervals: list[list[int]]) -> list[list[int]]:
+    if not intervals:
+        return []
+    
+    # Ordina la lista degli intervalli in base all'inizio
+    for i in range(len(intervals)):
+        for x in range(i + 1, len(intervals)):
+            if intervals[i][0] > intervals[x][0]:
+                intervals[i], intervals[x] = intervals[x], intervals[i]
+
+    merge = [intervals[0]]
+
+    for i in range(1, len(intervals)):
+        ultimo = merge[-1]
+        corrente = intervals[i]
+
+        if corrente[0] <= ultimo[1]:
+            ultimo[1] = max(ultimo[1], corrente[1])
+        else:
+            merge.append(corrente)
+
+    return merge
+
+lista = [[1, 3], [2, 6], [8, 10], [15, 18]]
+print(merge_intervals(lista))
--- javascriptesercizi/prima_app/src/componente1.js
+++ javascriptesercizi/prima_app/src/componente1.js
@@ -3,19 +3,13 @@ import React from 'react'
 const Componente1 = (props) => {
   console.log(props);
   return (
-  
     <>
-    <div></div>
     <div>componente1 di {props.children}</div>
-    <Anagrafica></Anagrafica>
-
-
-
   </>
-  );
-};
-  const Anagrafica=()=>
-      return(<div>Anagrafica<div/>)
+  )
+  const Anagrafica=()=>{
+
+  }
 }
 
 export default Componente1
